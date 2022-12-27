33426a173afb0178b7826e942007b18a8262fa77
saitejasangoju <teja@digitaldots.ai>
2022-12-27 22:37:45 +0530

    controller for customers

--- src/main/java/com/bank/controller/CustomerController.java
+++ src/main/java/com/bank/controller/CustomerController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.bank.dto.DepositAndWithdrawal;
 import com.bank.dto.MoneyTransfer;
 import com.bank.entity.Account;
 import com.bank.entity.Customer;
@@ -19,65 +17,64 @@ import com.bank.entity.Transaction;
 import com.bank.service.BankService;
 
 @RestController
-@RequestMapping("/api/v1/customer")
-public class BankController {
+@RequestMapping("/api/v1/customers")
+public class CustomerController {
 	
 	@Autowired
 	private BankService service;
 	
 	@GetMapping
 	public List<Customer> getAll() {
-		return service.getAll();
+		return service.getCustomers();
 	}
 
 	@PostMapping
 	public Customer create(@RequestBody Customer customer) throws Exception {
-		return service.create(customer);
+		return service.createCustomer(customer);
 	}
 	
-	@PutMapping("/{id}")
-	public Customer update(@PathVariable String id, @RequestBody Customer customer) {
-		return service.update(id, customer);
+	@PutMapping("/{customerId}")
+	public Customer update(@PathVariable String customerId, @RequestBody Customer customer) {
+		return service.updateCustomer(customerId, customer);
 	}
 	 
-	@DeleteMapping("/{id}")
-	public void delete(@PathVariable String customerId) {
-		service.delete(customerId);
+	@DeleteMapping("/{customerId}")
+	public Customer delete(@PathVariable String customerId) {
+		return service.deleteCustomer(customerId);
 	}
 	
-	@GetMapping("/account/{accountNumber}")
-	public Account getAccount(@PathVariable String accountNumber) throws Exception {
-		return service.getAccount(accountNumber);
+	@GetMapping("/{customerId}/accounts")
+	public List<Account> getCustomerAccounts(@PathVariable String customerId) throws Exception {
+		return service.getCustomerAccounts(customerId);
+	}
+	
+	@GetMapping("/{customerId}/accounts/{accountNumber}")
+	public Account getCustomerAccountByAccountNumber(@PathVariable String customerId, @PathVariable String accountNumber) throws Exception {
+		return service.getCustomerAccountByAccountNumber(customerId, accountNumber);
 	}
 
-	@PutMapping("/account/deposit")
-	public String deposit(@RequestBody DepositAndWithdrawal obj) throws Exception {
-		return service.deposit(obj);
+	@PutMapping("/{customerId}/accounts/{accountNumber}/deactivate")
+	public Account accountDeactivate(@PathVariable String customerId, @PathVariable String accountNumber) throws Exception {
+		return service.accountDeactivate(customerId, accountNumber);
 	}
 	
-	@PutMapping("/account/withdrawal")
-	public String withdrawal(@RequestBody DepositAndWithdrawal obj) throws Exception {
-		return service.withdrawal(obj);
+	@PutMapping("/{customerId}/accounts/{accountNumber}/activate")
+	public Account accountActivate(@PathVariable String customerId, @PathVariable String accountNumber) throws Exception {
+		return service.accountActivate(customerId, accountNumber);
 	}
 	
-	@PutMapping("/account/deactivate/{accountNumber}")
-	public String deActivate(@PathVariable String accountNumber) {
-		return service.deActivate(accountNumber);
-	}
-	
-	@PutMapping("/account/activate/{accountNumber}")
-	public String activate(@PathVariable String accountNumber) {
-		return service.activate(accountNumber);
-	}
-	
-	@GetMapping("/account/transaction/{accountNumber}")
-	public List<Transaction> getAllTransaction(@PathVariable String accountNumber) {
-		return service.getAllTransactions(accountNumber);
-	}
-	
-	@PutMapping("/account/transfer")
-	public String transfer(@RequestBody MoneyTransfer obj) throws Exception {
-		return service.moneyTransfer(obj);
+	@PostMapping("/{customerId}/accounts/{accountNumber}/deposit")
+	public Transaction deposit(@PathVariable String customerId, @PathVariable String accountNumber, @RequestBody Transaction transaction) throws Exception {
+		return service.deposit(customerId, accountNumber, transaction);
 	}
 
+	@PostMapping("/{customerId}/accounts/{accountNumber}/withdrawal")
+	public Transaction withdrawal(@PathVariable String customerId, @PathVariable String accountNumber, @RequestBody Transaction transaction) throws Exception {
+		return service.withdrawal(customerId, accountNumber, transaction);
+	}
+
+	@PostMapping("/{customerId}/accounts/{accountNumber}/transfer")
+	public List<Transaction> transfer(@PathVariable String customerId, @PathVariable String accountNumber, @RequestBody MoneyTransfer transferObj) throws Exception {
+		return service.moneyTransfer(transferObj);
+	}
 }
