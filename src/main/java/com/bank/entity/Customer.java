19fd9409a4ae9fda0282b22f3495e2d21d45df44
saitejasangoju <teja@digitaldots.ai>
2022-12-27 22:43:42 +0530

    customer entity to store customer details

--- src/main/java/com/bank/entity/Customer.java
+++ src/main/java/com/bank/entity/Customer.java
@@ -4,7 +4,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Generated;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
@@ -15,14 +14,11 @@ import lombok.ToString;
 @Document(collection="customer")
 public class Customer {
 	@Id
-	private String id;
+	private String customerId;
 	private String name;
 	private String dob;
-	private long phone;
+	private String phone;
 	private String email;
 	private String aadhar;
-	private Account account;
 	private Address address;
-	
-	
 }
