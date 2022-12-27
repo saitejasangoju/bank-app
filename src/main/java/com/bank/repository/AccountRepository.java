d526557b228d241f13fde624f17e618d1f00afda
saitejasangoju <teja@digitaldots.ai>
2022-12-27 22:38:58 +0530

    repository for accouts

---
+++ src/main/java/com/bank/repository/AccountRepository.java
@@ -0,0 +1,11 @@
+package com.bank.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.bank.entity.Account;
+
+public interface AccountRepository extends MongoRepository<Account, String>{
+
+	Account findByAccountNumber(String accountNumber);
+
+}
